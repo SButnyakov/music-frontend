30f227440c08460f93b017e8919290304f0418d8
Sergey Butnyakov <sergey.butnyakov@gmail.com>
2022-11-01 03:42:19 +0300

    xhr replaced with fetch

--- script/functions.js
+++ script/functions.js
@@ -2,15 +2,37 @@ let form = document.querySelector("form")
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    let xhr = new XMLHttpRequest();
-    xhr.open(form.method, form.action, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-    xhr.setRequestHeader('Access-Control-Allow-Credentials', 'true');
-    xhr.withCredentials = true
     let data = {
         "login":`${form.login.value}`,
         "password": `${form.password.value}`
     }
-    xhr.send(JSON.stringify(data))
+    onFormSubmit(data)
+    .then(answer => {
+        if (answer[1] === 200) {
+            window.location.replace("http://localhost:80/")
+        } else if (answer[1] === 400 || answer[1] === 401) {
+            document.querySelector("h1")
+            .insertAdjacentHTML("afterend", "<p style=\"color: red\">Неверно</p>")
+        } else if (answer[1] === 500) {
+            document.querySelector("h1")
+            .insertAdjacentHTML("afterend", "<p style=\"color: red\">Неверно</p>")
+        }
+    })
 })
+
+async function onFormSubmit(data) {
+    let response = await fetch(form.action, {
+        method: form.method,
+        body: JSON.stringify(data),
+        mode: 'cors',
+        headers: new Headers({
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Access-Control-Allow-Origin': 'http://localhost:8080',
+            'Access-Control-Allow-Credentials': 'true'
+        }),
+        credentials: 'include',
+    });
+    let answer = await response.json()
+    let code = response.status
+    return [answer, code]
+}
